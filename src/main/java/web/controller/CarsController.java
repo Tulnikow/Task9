b1c74c4a8f8252dff2fe8b2691db0eee30afb676
Andrei <tulnikow@yandex.ru>
2022-10-07 18:49:14 +0300

    3

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -6,16 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.services.ServiceCar;
 import web.services.ServiceCarImpl;
 
 @Controller
 public class CarsController {
+    private ServiceCar service;
+
     @Autowired
-    private  ApplicationContext context;
+    public CarsController(ServiceCar service) {
+        this.service = service;
+    }
 
     @GetMapping("/cars")
     public String cars(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
-        ServiceCarImpl service = context.getBean(ServiceCarImpl.class);
         model.addAttribute("list", service.getCarByCount(count));
         return "cars";
     }
